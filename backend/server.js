fa2dd209780c688a601c68ff1c948f3bb7fbe1be
Vedant <vedantagrawal31816@gmail.com>
2025-10-29 21:42:05 +0530

    deploy process 2

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));dotenv.config();
+app.use(
+  cors({
+    origin: ["https://nexora-assign.vercel.app", "http://localhost:5173"],
+  })
+);
+
+dotenv.config();
 connectDb();
 
 app.get("/", (req, res) => {
